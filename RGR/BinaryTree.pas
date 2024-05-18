d893f867d57126c1ab5de8039d66bf5d71d3c097
Alexander Smirnov <aleksander50.500@gmail.com>
2024-05-18 11:34:24 +0300

    Сделал ограничение на длинну уникального слова

--- RGR/BinaryTree.pas
+++ RGR/BinaryTree.pas
@@ -6,7 +6,8 @@ USES
   WordReader; 
 
 CONST
-  MaxWords = 10000;
+  MaxInputWords = 10000;
+  MaxWordsCount = 10; //уникальные слова
   
 TYPE 
   Tree = ^NodeType;
@@ -16,6 +17,9 @@ TYPE
                 LLink, RLink: Tree;
              END;
                
+VAR
+  WordsCount: INTEGER;
+
 PROCEDURE CountWordsFromFile(VAR InputFile: TEXT; VAR Root: Tree);
 PROCEDURE PrintWordCount(Ptr: Tree; VAR OutputFile: TEXT);
 
@@ -27,78 +31,74 @@ VAR
 BEGIN
   Len1 := Length(Word1);
   Len2 := Length(Word2);
-  
-  IF Len1 < Len2 
-  THEN
-    MinLen := Len1
-  ELSE
+  MinLen := Len1;
+  IF Len1 > Len2 THEN
     MinLen := Len2;
 
   i := 1;
-  WHILE (i <= MinLen) AND (Word1[i] = Word2[i]) 
-  DO
+  WHILE (i <= MinLen) AND (Word1[i] = Word2[i]) DO
     i := i + 1;
 
-  IF (i <= MinLen) 
-  THEN
-    BEGIN
-      IF Word1[i] < Word2[i] 
-      THEN
-        CompareWords := -1
-      ELSE
-        CompareWords := 1;
-    END
+  IF i <= MinLen THEN
+  BEGIN
+    IF Word1[i] < Word2[i] THEN
+      CompareWords := -1
+    ELSE
+      CompareWords := 1;
+  END
   ELSE
-    BEGIN
-      IF Len1 < Len2 
-      THEN
-        CompareWords := -1
-      ELSE 
-        IF Len1 > Len2 
-        THEN
-          CompareWords := 1
-        ELSE
-          CompareWords := 0;
-    END;
+  BEGIN
+    IF Len1 < Len2 THEN
+      CompareWords := -1
+    ELSE IF Len1 > Len2 THEN
+      CompareWords := 1
+    ELSE
+      CompareWords := 0;
+  END;
 END;
 
 PROCEDURE InsertWord(VAR Ptr: Tree; NewWord: STRING);
 BEGIN
-  IF Ptr = NIL 
-  THEN
-    BEGIN
-      NEW(Ptr);
-      Ptr^.Word := NewWord;
-      Ptr^.Count := 1;
-      Ptr^.LLink := NIL;
-      Ptr^.RLink := NIL;
-    END
+  IF Ptr = NIL THEN
+  BEGIN
+    NEW(Ptr);
+    Ptr^.Word := NewWord;
+    Ptr^.Count := 1;
+    Ptr^.LLink := NIL;
+    Ptr^.RLink := NIL;
+    WordsCount := WordsCount + 1;
+  END
   ELSE
-    BEGIN
-      CASE CompareWords(NewWord, Ptr^.Word) OF
-        -1: InsertWord(Ptr^.LLink, NewWord);
-        1: InsertWord(Ptr^.RLink, NewWord);
-        0: Ptr^.Count := Ptr^.Count + 1;
-      END;
+  BEGIN
+    CASE CompareWords(NewWord, Ptr^.Word) OF
+      -1: InsertWord(Ptr^.LLink, NewWord);
+      1: InsertWord(Ptr^.RLink, NewWord);
+      0: IF Ptr^.Count < MaxWordsCount 
+         THEN
+           Ptr^.Count := Ptr^.Count + 1;
     END;
+  END;
 END;
 
 PROCEDURE CountWordsFromFile(VAR InputFile: TEXT; VAR Root: Tree);
 VAR
   Word: STRING;
   Length: INTEGER; 
-  WordCount: INTEGER;  
+  InputWordsCount: INTEGER;
 BEGIN         
   RESET(InputFile);
-  WordCount := 0;
-  WHILE (NOT EOF(InputFile)) AND (WordCount < MaxWords)  
+  WordsCount := 0; 
+  InputWordsCount := 0;
+  WHILE NOT EOF(InputFile) AND (InputWordsCount < MaxInputWords) 
   DO
     BEGIN
       ReadWordsFromFile(InputFile, Word, Length);
       IF Length > 0 
       THEN
-        InsertWord(Root, Word);
-        WordCount := WordCount + 1;  
+        BEGIN
+          InsertWord(Root, Word);
+          InputWordsCount := InputWordsCount + 1
+        END;
     END;            
 END;
 
