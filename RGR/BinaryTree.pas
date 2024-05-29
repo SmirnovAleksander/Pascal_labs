1cbeb2f4ae2774d5d13d9637db85fa5ba3cdefd3
SmirnovAleksander <73950970+SmirnovAleksander@users.noreply.github.com>
2024-05-29 04:10:17 +0300

    edit rgr last

--- RGR/BinaryTree.pas
+++ RGR/BinaryTree.pas
@@ -7,7 +7,7 @@ USES
 
 CONST
   MaxInputWords = 10000;
-  MaxWordsCount = 10; // уникальные слова
+  MaxWordsCount = 10; 
 
 TYPE
   Tree = ^NodeType;
@@ -26,95 +26,92 @@ PROCEDURE PrintTree(Ptr: Tree; VAR OutputFile: TEXT);
 
 IMPLEMENTATION
 
+
+FUNCTION FilterChar(c: CHAR): CHAR;
+  BEGIN
+    IF c = 'ё' 
+    THEN
+      FilterChar := 'е'
+    ELSE
+      FilterChar := c
+  END
+
 FUNCTION CompareWords(Word1, Word2: STRING): INTEGER;
 VAR
   Len1, Len2, MinLen, i: Integer;
   Char1, Char2: CHAR;
 
-  FUNCTION AdjustChar(c: CHAR): CHAR;
-  BEGIN
-    IF c = 'ё' THEN
-      AdjustChar := 'е' // Логически перемещаем 'ё' после 'е'
-    ELSE
-      AdjustChar := c;
-  END;
-
 BEGIN
   Len1 := Length(Word1);
   Len2 := Length(Word2);
   MinLen := Len1;
-  IF Len1 > Len2 THEN
+
+  IF Len1 > Len2 
+  THEN
     MinLen := Len2;
 
   i := 1;
   WHILE (i <= MinLen) DO
   BEGIN
-    Char1 := AdjustChar(Word1[i]);
-    Char2 := AdjustChar(Word2[i]);
+    Char1 := FilterChar(Word1[i]);
+    Char2 := FilterChar(Word2[i]);
 
-    IF Char1 < Char2 THEN
-    BEGIN
+    IF Char1 < Char2 
+    THEN
       CompareWords := -1;
-      i := MinLen + 1; // Прерываем цикл
-    END
-    ELSE IF Char1 > Char2 THEN
-    BEGIN
-      CompareWords := 1;
-      i := MinLen + 1; // Прерываем цикл
-    END
-    ELSE
-    BEGIN
-      // Если буквы равны, учитываем, что 'ё' идет после 'е'
-      IF (Word1[i] = 'ё') AND (Word2[i] <> 'ё') THEN
-      BEGIN
+    ELSE 
+      IF Char1 > Char2 
+      THEN
         CompareWords := 1;
-        i := MinLen + 1; // Прерываем цикл
-      END
-      ELSE IF (Word1[i] <> 'ё') AND (Word2[i] = 'ё') THEN
-      BEGIN
-        CompareWords := -1;
-        i := MinLen + 1; // Прерываем цикл
-      END
       ELSE
-      BEGIN
-        CompareWords := 0; // Сравниваем следующие символы
-      END;
-    END;
+        IF (Word1[i] = 'ё') AND (Word2[i] <> 'ё') 
+        THEN
+          CompareWords := 1;
+        ELSE 
+          IF (Word1[i] <> 'ё') AND (Word2[i] = 'ё') 
+          THEN
+            CompareWords := -1;
+          ELSE
+            CompareWords := 0; 
+
     i := i + 1;
   END;
 
-  // Если все символы равны до длины более короткого слова
-  IF CompareWords = 0 THEN
-  BEGIN
-    IF Len1 < Len2 THEN
+  IF CompareWords = 0 
+  THEN
+    IF Len1 < Len2 
+    THEN
       CompareWords := -1
-    ELSE IF Len1 > Len2 THEN
-      CompareWords := 1
-    ELSE
-      CompareWords := 0;
-  END;
+    ELSE 
+      IF Len1 > Len2 
+      THEN
+        CompareWords := 1
+      ELSE
+        CompareWords := 0;
 END;
 
 PROCEDURE InsertWord(VAR Ptr: Tree; NewWord: STRING);
 BEGIN
-  IF Ptr = NIL THEN
-  BEGIN
-    NEW(Ptr);
-    Ptr^.Word := NewWord;
-    Ptr^.Count := 1;
-    Ptr^.LLink := NIL;
-    Ptr^.RLink := NIL;
-    WordsCount := WordsCount + 1
-  END
-  ELSE
-  BEGIN
-    CASE CompareWords(NewWord, Ptr^.Word) OF
-      -1: InsertWord(Ptr^.LLink, NewWord);
-      1: InsertWord(Ptr^.RLink, NewWord);
-      0: IF Ptr^.Count < MaxWordsCount THEN
-           Ptr^.Count := Ptr^.Count + 1
+  IF Ptr = NIL 
+  THEN
+    BEGIN
+      NEW(Ptr);
+      Ptr^.Word := NewWord;
+      Ptr^.Count := 1;
+      Ptr^.LLink := NIL;
+      Ptr^.RLink := NIL;
+      WordsCount := WordsCount + 1
     END
-  END
+  ELSE
+    BEGIN
+      CASE CompareWords(NewWord, Ptr^.Word) OF
+        -1: InsertWord(Ptr^.LLink, NewWord);
+        1: InsertWord(Ptr^.RLink, NewWord);
+        0: IF Ptr^.Count < MaxWordsCount 
+           THEN
+             Ptr^.Count := Ptr^.Count + 1
+      END
+    END
 END;
 
 PROCEDURE CountWordsFromFile(VAR InputFile: TEXT);
@@ -127,25 +124,28 @@ BEGIN
   Root := NIL;
   WordsCount := 0;
   InputWordsCount := 0;
-  WHILE NOT EOF(InputFile) AND (InputWordsCount < MaxInputWords) DO
-  BEGIN
-    ReadWordsFromFile(InputFile, Word, Length);
-    IF Length > 0 THEN
+  WHILE NOT EOF(InputFile) AND (InputWordsCount < MaxInputWords) 
+  DO
     BEGIN
-      InsertWord(Root, Word);
-      InputWordsCount := InputWordsCount + 1
+      ReadWordsFromFile(InputFile, Word, Length);
+      IF Length > 0 
+      THEN
+        BEGIN
+          InsertWord(Root, Word);
+          InputWordsCount := InputWordsCount + 1
+        END
     END
-  END
 END;
 
 PROCEDURE PrintTree(Ptr: Tree; VAR OutputFile: TEXT);
 BEGIN
-  IF Ptr <> NIL THEN
-  BEGIN
-    PrintTree(Ptr^.LLink, OutputFile);
-    WRITELN(OutputFile, Ptr^.Word, ' ', Ptr^.Count);
-    PrintTree(Ptr^.RLink, OutputFile);
-  END
+  IF Ptr <> NIL 
+  THEN
+    BEGIN
+      PrintTree(Ptr^.LLink, OutputFile);
+      WRITELN(OutputFile, Ptr^.Word, ' ', Ptr^.Count);
+      PrintTree(Ptr^.RLink, OutputFile);
+    END
 END;
 
 END.
